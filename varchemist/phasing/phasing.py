d7399c2dc81c2bc52ffefe15725649727ba03e51
Judson.x.Belmont <budsonjelmont@gmail.com>
2022-06-17 18:50:25 -0400

    Adjust import statements for new vargrouper package structure

--- varchemist/phasing/phasing.py
+++ varchemist/phasing/phasing.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
-from celltics.tools import vargroup, datasource
+from celltics.tools import vargroup
+from celltics.datasource import seqrepo
 from varchemist import utils
 
 def merge_phased_vars(vcf, build, merge_distance=500):
@@ -9,9 +10,8 @@ def merge_phased_vars(vcf, build, merge_distance=500):
   print('vars_skipped:') # TODO if any, need to error out here
   print([vargroup.get_id(v) for v in vars_skipped])
   n_candidates = len(vars_to_group) # For logging
-  sr = datasource.SeqRepo('http://127.0.0.1:7777', build)
+  sr = seqrepo.SeqRepo('http://127.0.0.1:7777', build)
   records, var_dict, skipped = vargroup.bam_and_merge_multiprocess(None, vars_to_group, fq_threshold=0, min_reads=99999, bam_filter_mode='pagb', ref_seq=None, nthreads=1, debug=False, datasource=sr)
   df = utils.vcf_to_df(records)
   df['skipped'] = skipped
   return df
-
