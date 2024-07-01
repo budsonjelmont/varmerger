95f78f87d1216b18896e830de85f5688b6e1d38a
Judson Belmont <budsonjelmont@gmail.com>
2024-06-30 22:59:24 -0400

    Remove some unnecessary code

--- app/flask/api/api.py
+++ app/flask/api/api.py
@@ -47,19 +47,9 @@ def test():
 ### Run VarGrouper to produce merged VCF ###
 @api.route('/phase/<string:build>/merge', methods=methodspermitted)
 def merge(build):
-    # NEW WIP
     data = request.get_json(silent=False, force=False)
-    # try:
-    print('build:', build)
-    print('data:', data)
     result = service.process_merge(build, data)
-    # Return a successful response with the result
     return result, 200, {'ContentType':'application/json'} 
-    # except NotFound as e: # TODO is this needed?
-    #     return str(e), 404
-    # except BadRequest:
-    #     return 'Bad request', 400
-    # Handle other exceptions and return appropriate responses
 
 ##############################################
 ##############################################
